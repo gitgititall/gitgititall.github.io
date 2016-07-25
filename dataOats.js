e199667a513855946613ac469299b01e62ffb5b3
gitgititall <mfarahu+github@gmail.com>
2016-07-25 15:56:58 +0800

    Update dataOats.js

--- dataOats.js
+++ dataOats.js
@@ -26,54 +26,78 @@ deps=[
 	],
 	TAG: '#p pst'
 },
-{AIM: ' Develop new business opportunities', OBJS:[
-{OBJ: '16 Meet people, exchange ideas and build contacts (pma) ((nextlevel))',TAS:[
-'>>meet sahoo, apologise. trip to seletar cafe.',
-'>>meet dwee',
-'meet ck Jorge Kazmi dmcd StanArch SAS-Surojit Sandipan niloo sunilkumarSCB datasci! jpm2 rajivshetty sahoo dhruba afees',
-]},
-{OBJ: '25 Focus on big data, find a way, get real experience, not just talking about it (pma). Its all about Big Data (pma).',TAS:[
-]},
-{OBJ: '((SwitchThisYear)) (pma) ((Raise the game, remember you are an IITian! else only 10-15% (pma) ))',TAS:[
-'>> check with Popsi, then contact Sahoo / Nimish, proper closure',
-]},
-{OBJ: '25 meet dmcd and get his inputs on ML/SPARK, show your "results"',TAS:[
-'>> fix the ML presentation (encode Popsi tips on ppt, save Smart Cities ppt)',
-]},
-{OBJ: '0 next level - be able to do the next two levels of work ((next level))',TAS:[
-'??',
-]},
-],TAG: '#s pst'},
-{AIM: ' Speak and write about my ideas', OBJS:[
-{OBJ: '20 complete Stanford ml course and then do google deep learning course on udemy ((Have real machine learning experience, not just talking about it (pma). Ticket to Fintech.))',TAS:[
-'>>complete 3/18 (Unsupervised Learning, SVM, Logistic Regression) quizzes of stanford ml',
-'complete remaining quizzes of stanford ml, total 18.',
-]},
-{OBJ: '16 read 10 books and attend safari conferences. ((Be most informed to get the edge. Know at research level about banking technology architecture (pma). Read a lot!))',TAS:[
-'>> read pozen style -- Applied Business Analytics: Integrating Business Process, Big Data, and Advanced Analytics (Ch 7 ++)',
-'>> read pozen style -- Designing Data-Intensive Applications (Chapter 3. Storage and Retrieval) (Chapter 4. Encoding and Evolution)',
-]},
-{OBJ: '20 Write a whitepaper on Banking Architecture. Take thinking to next level (pma) ((Be most informed to get the edge. Know at research level about banking technology architecture (pma). Read a lot!))',TAS:[
-'finalize the table of contents',
-'>> see Challenges to the future of Retail Banking slideshare',
-]},
-{OBJ: '16 Build a Cordova App on macbook - base for digital',TAS:[
-'5am on weekend',
-]},
-{OBJ: '20 Master Docker basics, deliver talk',TAS:[
-'>>incorporate Docker history',
-'>>watch Docker talks',
-]},
-],TAG: '#t pst'},
+{AIM: ' Develop new business opportunities', 
+	OBJS:[
+	{OBJ: '16 Meet people, exchange ideas and build contacts (pma) ((nextlevel))',
+		TAS:[
+		'>>meet sahoo, apologise. trip to seletar cafe.',
+		'>>meet dwee',
+		'meet ck Jorge Kazmi dmcd StanArch SAS-Surojit Sandipan niloo sunilkumarSCB datasci! jpm2 rajivshetty sahoo dhruba afees',
+		]
+	},
+	{OBJ: '25 Focus on big data, find a way, get real experience, not just talking about it (pma). Its all about Big Data (pma).',
+		TAS:[
+		]
+	},
+	{OBJ: '((SwitchThisYear)) (pma) ((Raise the game, remember you are an IITian! else only 10-15% (pma) ))',
+		TAS:[
+		'>> check with Popsi, then contact Sahoo / Nimish, proper closure',
+		]
+	},
+	{OBJ: '25 meet dmcd and get his inputs on ML/SPARK, show your "results"',
+		TAS:[
+		'>> fix the ML presentation (encode Popsi tips on ppt, save Smart Cities ppt)',
+		]
+	},
+	{OBJ: '0 next level - be able to do the next two levels of work ((next level))',
+		TAS:[
+			'??',
+		]
+	},
+	],TAG: '#s pst'},
+{AIM: ' Speak and write about my ideas', 
+	OBJS:[
+	{OBJ: '20 complete Stanford ml course and then do google deep learning course on udemy ((Have real machine learning experience, not just talking about it (pma). Ticket to Fintech.))',
+		TAS:[
+		'>>complete 3/18 (Unsupervised Learning, SVM, Logistic Regression) quizzes of stanford ml',
+		'complete remaining quizzes of stanford ml, total 18.',
+		]
+	},
+	{OBJ: '16 read 10 books and attend safari conferences. ((Be most informed to get the edge. Know at research level about banking technology architecture (pma). Read a lot!))',
+		TAS:[
+		'>> read pozen style -- Applied Business Analytics: Integrating Business Process, Big Data, and Advanced Analytics (Ch 7 ++)',
+		'>> read pozen style -- Designing Data-Intensive Applications (Chapter 3. Storage and Retrieval) (Chapter 4. Encoding and Evolution)',
+		]
+	},
+	{OBJ: '20 Write a whitepaper on Banking Architecture. Take thinking to next level (pma) ((Be most informed to get the edge. Know at research level about banking technology architecture (pma). Read a lot!))',
+		TAS:[
+		'finalize the table of contents',
+		'>> see Challenges to the future of Retail Banking slideshare',
+		]
+	},
+	{OBJ: '16 Build a Cordova App on macbook - base for digital',
+		TAS:[
+		'5am on weekend',
+		]},
+	{OBJ: '20 Master Docker basics, deliver talk',
+		TAS:[
+		'>>incorporate Docker history',
+		'>>watch Docker talks',
+		]
+	},
+	],TAG: '#t pst'},
 {AIM: ' Nurture family and self', OBJS:[
-{OBJ: 'Dress well and arrive on time with pomp ((Be physically fit; lose the paunch; be healthy and full of vigor (pma)))',TAS:[
+	{OBJ: 'Dress well and arrive on time with pomp ((Be physically fit; lose the paunch; be healthy and full of vigor (pma)))',TAS:[
 '>> exercise for 60 minutes',
 ]},
-{OBJ: 'Apply the tools (courage, urgency and flow) and (love & gratitude) and achieve 100% nmz',TAS:[
-]},
-{OBJ: 'Have palpable intensity and urgency. Be visible, 5 ppl/day. Forward motion radiating outwards (pma)',TAS:[
-]},
-],TAG: '#ame amen'},
+	{OBJ: 'Apply the tools (courage, urgency and flow) and (love & gratitude) and achieve 100% nmz',TAS:[
+	]
+	},
+	{OBJ: 'Have palpable intensity and urgency. Be visible, 5 ppl/day. Forward motion radiating outwards (pma)',TAS:[
+	]
+	},
+	],TAG: '#ame amen'
+},
 {AIM: ' Build the platform', OBJS:[
 {OBJ: '20 invest in sg, india ((investment platform))',TAS:[
 '>>store cdp acct num - ask for internet access',
