437b519b29b74257a9ad8e28e302389ee1927352
gitgititall <gitgititall@users.noreply.github.com>
2019-08-30 15:00:37 +0800

    Create theta2.js

---
+++ mt/generic_runner/theta2.js
@@ -0,0 +1,89 @@
+var strs ={
+
+
+rem : '\
+||: write 1000 words to clear the mind |...\
+||: do the hardest stuff first! <br> out of time!| ...\
+||: don’t read/watch to consume. be creating content in parallel. blog and present like hell! move up to conference speeches next.|...\
+||: don’t read – first write 10 or more questions and then answer them, and blog if possible! writing the questions clarifies everything! be a great writer of questions.|...\
+||: javascript based trainer (mt mobile trainer) works on mobile and is great for learning the tools as well as any technical stuff. it can help change the default response. it can help in applying the reversal of desire.|...\
+||: apply the tools! this is extremely useful. have been using active love in meetings. and the shadow. superb stuff. and the idea of intensity, doing the hardest things first. also reversal of desire, by feeling the pain physically and entering that space, learning to love it, and moving out of the comfort zone.|...\
+||: you always have 5 mins. or 10 mins. for something. and most things can be done in 10 mins! especially the painful things being avoided, ninja style! this could be dishes or laundry or cooking or email or analysis!|...\
+||: focus on what needs to be done in the next hour – but do things that will build the platform for the next many years.|...\
+||: start with the hypothesis. don’t keep adding information, instead, have a hypothesis (pozen style) and work towards it.|...\
+||: problems are provided by higher force to help you grow and transform. seize the opportunities.|...||: focus on sleeping well. else nothing else works.|...\
+',
+mins6: '\
+|| declutter 6 |   \
+|| poems invest 6 |   \
+|| motilal oswal 6 |   \
+|| plank 3 and burpees 3 |   \
+|| kettlebells 6 |   \
+|| 10-20-30 run 6 + 6 |   \
+|| mop out 6 |   \
+|| ramdev-kapalb 6 |   \
+|| nmz 6 |    \
+|| sleep descent 6|    \
+|| 10 questions 6 |    \
+|| active love 3 // reversal of des 3 |    \
+|| atta 6 | \
+',
+
+now_thinking : '\
+||what is event api? | see the manual! |\
+||what is session api? | please ... see the manual! |\
+',
+
+tools1: '\
+||Buying house ... got no time to pay attention |...\
+||School admissions ... got no time to pay attention |...\
+||NOT EVEN 10 MINS ON SOMETHING USEFUL!!!! Learn Aurdiono!!!!|...\
+||Use Arvinds account |...\
+||Allocate lockers to Giddi and Shailesh |...\
+||Bonding with better half .... OpenView |...\
+||Walk in the rain |...\
+||Kids out of control ... restore order |...\
+||Too tired ...11 pm ... time for watching serial or youtube |...\
+||Use trunk ... will do tomorrow |...\
+||Agarwal math is tough! ... will do tomorrow ... trunk |...\
+||Fix the timer first |...\
+||Curses to Gid for cornering business |...\
+||10pm ... no time for nmz ... no time for 12 min workout |...\
+||Escalator at rp |...\
+||Ameruddin roza ... cute? |...\
+||Buffet and little india ... tomorrows office ... |...\
+||My slides taken away … no space |...\
+||Bad natured! DONT BE RUDE TO MASHA!|...\
+||lets get new workouts ... new links ... new reading material |...\
+||house dirty ... need to clean till 12:30AM |...\
+||tap broken? let sesha pay! | ...\
+||must pay 300 for swimming! | ...\
+',
+
+tools_rod: '\
+|| singtel bill | \
+|| my insurance |   \
+|| roti maker |   \
+|| video conf |   \
+|| air con |   \
+|| save counsellor advice |   \
+|| talk to khalu and f&f |   \
+|| DL for group |   \
+',
+
+HINDI:'\
+|| नाना | \
+|| दादा | \
+|| पापा | \
+|| मामा | \
+|| लाला | \
+|| बाबा | \
+',
+
+
+
+}
+
+speed = {
+	tools_rod:'FAST',
+}
