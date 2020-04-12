e0e721f1cd42b970489404350050ea1df4f25ac7
gitgititall <gitgititall@users.noreply.github.com>
2020-04-13 00:51:24 +0800

    Create wisdom.js

---
+++ mt/generic_runner/wisdom.js
@@ -0,0 +1,12 @@
+var strs = {
+
+daily_wisdom:'\
+||Anyone who curses others is cursed himself. (M)|...\
+||The Prophet (SAW) was told about a woman who used to insult neighbours with offensive comments. He dubbed her an inmate of Hell even though she was well known for her devotion to prayer, fasting and charity.(A/BQ)|...\
+||Treat your children with kindness and affection. Take care of their moral upbringing. (I)|...\
+||The best ones among you are those who treat their wives well. A perfect believer is he who is of excellent character. (B).|...\
+||Do not indulge in backbiting. Nor should you pry into or publicize the failings of others. Whoever is guilty of it ... is bound to be disgraced publicly. (D)|...\
+||Those of you who have excellent conduct will enjoy my company, but those with ill-manners and sharp, reproaching tongues will be kept away from me. (BQ/MSH)|...\
+||"Do not lose your temper" said the Prophet (SAW) when asked for advice. When asked for more advice he repeated "Do not lose your temper"|...\
+',
+}
